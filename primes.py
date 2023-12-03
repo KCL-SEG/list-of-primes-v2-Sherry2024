9f577fa79be73ca9dd235a71e24f96a76b003cbb
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2023-12-03 15:48:24 +0000

    Add raise

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+def is_prime(int):
+    if(int < 2):
+        return False
+    i = 2
+    while i < int*0.5+1: 
+        if int % i ==0:
+            return False
+        else:
+            i += 1
+    return True
+
+
 
 def primes(number_of_primes):
     list = []
-    return list
+    num = 2 # first prime number 
+
+    if number_of_primes <= 0:
+        raise ValueError (f"Number should be a positive number")
+    else:
+        while len(list) < number_of_primes:
+            if is_prime(num):
+                list.append(num)
+                num += 1
+        return list
